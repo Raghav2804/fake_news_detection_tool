aeff63d20aa591d8847d9077edf2f1238800cd79
Raghav2804 <iamraghav28@gmail.com>
2025-05-01 10:37:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -270,6 +270,6 @@ def get_results():
     except Exception as e:
         return jsonify({'error': f'Error fetching results: {str(e)}'}), 500
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
